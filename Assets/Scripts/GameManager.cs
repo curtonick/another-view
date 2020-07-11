8bd6ed20fe12c3a331f28907080f58839e2304b9
curtonick <curtonick@gmail.com>
2020-07-11 09:38:26 -0500

    Duplicated version of Pun Basics demo as a starting point

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Aview
+{
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager _Instance = null;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            if (_Instance == null)
+            {
+                _Instance = this;
+            } 
+            else
+            {
+                Debug.LogError("There is already a GameManager Instance!");
+            }
+        }
+
+        public static void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+    }
+}
---
+++ Assets/Scripts/TitleScreenUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Aview
+{
+    public class TitleScreenUI : MonoBehaviour
+    {
+        [SerializeField]
+        Button m_StartGameButton = null;
+        [SerializeField]
+        Button m_OptionButton1 = null;
+        [SerializeField]
+        Button m_OptionButton2 = null;
+        [SerializeField]
+        Button m_QuitGame = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_StartGameButton.onClick.AddListener(() => GameManager.LoadScene("Launcher"));
+            m_QuitGame.onClick.AddListener(() => Application.Quit());
+        }
+
+    }
+}
