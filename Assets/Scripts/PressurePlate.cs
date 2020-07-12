6ad5fec50b2cd9d71300c05009aec4d9150de994
Kyle Hutchinson <kyle.hutchinson@ncr.com>
2020-07-11 23:53:09 -0400

    Add button and door

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Collider plate;
+    public GameObject door;
+
+    private bool isClosed = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        door.SetActive(isClosed ? false : true);
+        isClosed = !isClosed;
+    }
+}
